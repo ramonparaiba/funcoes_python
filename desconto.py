9ee9270366908e2b52b4902063f1ad09ac4ce51e
ramonparaiba <ramonparaiba@gmail.com>
2025-04-03 11:00:09 -0300

    feat: exercicios

---
+++ desconto.py
@@ -0,0 +1,6 @@
+valor = float(input('digite o valor: '))
+
+def calcula_desconto(valor):
+    print(valor * 0.95)
+
+calcula_desconto(valor)
---
+++ exercicios.py
@@ -0,0 +1,12 @@
+numero = int(input('digite um numero '))
+def dobro (num):
+    print(num*2)
+dobro(numero)
+
+def par_ou_impar(num):
+    if num % 2 == 0:
+        print('é par')
+    else:
+        print('é impar')
+
+par_ou_impar(numero)
